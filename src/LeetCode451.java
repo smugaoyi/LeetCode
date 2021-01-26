2c0981ff9bcb82ae9de540dff7d5828a6ce7149e
gaoyi <gaoyi@wbpharma.com>
2021-01-26 16:17:42 +0800

    451

---
+++ src/LeetCode451.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class LeetCode451 {
+
+    static class Solution {
+
+        // 此类问题也可以看作时查找问题, 从一个字符串中找出出现频率最高的字符, 然后拍在最前面
+        // 使用Java 提供的优先队列来按照出现次数进行排序
+        public String frequencySort(String s) {
+
+            if (s == null || s.length() < 2) return s;
+
+            // 存储字符/对应字符出现次数
+            int[] frequency = new int[128];
+            char[] chars = s.toCharArray();
+
+            // 计算每个字符出现次数
+            for (int i = 0; i < chars.length; i++) {
+                frequency[chars[i]]++;
+            }
+
+            // 定义一个大顶堆
+            PriorityQueue<Character> heap2 = new PriorityQueue<>(new Comparator<Character>() {
+                public int compare(Character a, Character b) {
+                    return frequency[b] - frequency[a];
+                }
+            });
+
+            // 将数据写入到PriorityQueue中
+            for (int j = 0; j < 128; j++) {
+                if (frequency[j] != 0)
+                    heap2.offer((char) j);
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (!heap2.isEmpty()) {
+                char ch = heap2.poll();
+                while (frequency[ch] > 0) {
+                    stringBuilder.append(ch);
+                    frequency[ch]--;
+                }
+            }
+
+            return stringBuilder.toString();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        String res = solution.frequencySort("ssaatbbb");
+        System.out.println(res);
+    }
+
+}
