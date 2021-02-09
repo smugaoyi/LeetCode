043841cfd529d3d6ca14882247b3951c829b2338
gaoyi <gaoyi@wbpharma.com>
2021-02-09 10:46:37 +0800

    435-重复区间的个数

--- src/LCode435.java
+++ src/LCode435.java
@@ -1,27 +1,44 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * 不重叠区间的个数：
+ * 先计算出不重叠的区间的个数，然后用区间的总数减去不重叠的区间的个数，即需要移除的区间的个数
+ */
 public class LCode435 {
 
+    static class Solution {
+        public int eraseOverlapIntervals(int[][] intervals) {
+            if (intervals.length == 0) {
+                return 0;
+            }
+
+            Arrays.sort(intervals, new Comparator<int[]>() {
+                public int compare(int[] interval1, int[] interval2) {
+                    return interval1[1] - interval2[1];
+                }
+            });
+
+            int n = intervals.length;
+            int right = intervals[0][1];
+            int ans = 1;
+            for (int i = 1; i < n; ++i) {
+                if (intervals[i][0] >= right) {
+                    ++ans;
+                    right = intervals[i][1];
+                }
+            }
+            return n - ans;
+        }
+    }
 
     public static void main(String[] args) {
 
-        int[][] arr = {{1, 2}, {2, 3}, {3, 4}};
+        int[][] arr = {{1, 2}, {2, 3}, {2, 4}};
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[1] - o1[1];
-            }
-        });
+        Solution solution = new Solution();
+        int i = solution.eraseOverlapIntervals(arr);
+        System.out.println(i);
 
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("-----");
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.println(arr[i][j]);
-            }
-
-        }
     }
 }
